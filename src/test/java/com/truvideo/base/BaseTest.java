2e7436f4f1773053c7227978cd97e48590dcd8c2
Suraj5excep <suraj.kushwah@5exceptions.com>
2024-12-19 17:01:21 +0530

    surajs

--- src/test/java/com/truvideo/base/BaseTest.java
+++ src/test/java/com/truvideo/base/BaseTest.java
@@ -29,7 +29,7 @@ public class BaseTest {
 	@Parameters({ "browser", "headless", "env" })
 	public void loginPageSetup(
 			@Optional("chrome") String browser, 
-			@Optional("false") String headless,
+			@Optional("") String headless,
 			@Optional("stagingrc") String env)
 			{
 
